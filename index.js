5e94f6d0666f31dbfe5f171940d0966bfd84e91b
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-05-13 22:05:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function() {
 	const params = new URLSearchParams(accessURL.search);
 	const domain = (params.get('host') || '');
 	const path = params.get('path') || '';
-	const url = 'https://'+'domain'+'yuameshi.top'+path;
+	const url = 'https://'+domain+'yuameshi.top'+path;
 	document.querySelector('div#detectDomain').innerHTML=
 	'检测到您的原始访问路径为：https://<font color="yellow">'+domain+'</font>han-han.xyz<font color="yellow">'+path+'</font><br/>'+
 	'将为您重定向到定向到<a href="'+url+'">https://<font color="yellow">'+domain+'</font>yuameshi.top<font color="yellow">'+path+'</font></a>';
